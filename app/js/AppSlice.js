5007b0ecff9e18115756d80e44fa75d4a08dcc6d
Eran Geva <eran@eran-mac.local>
2018-07-21 15:10:26 +0300

    app is slice

--- app/js/AppSlice.js
+++ app/js/AppSlice.js
@@ -31,8 +31,6 @@ class AppSlice {
     initGui() {
         this.applyGuiChanges = this.applyGuiChanges.bind(this);
         this.gui = new dat.GUI({ autoPlace: true, width: 500 });
-        this.app = 'Slice';
-        this.gui.add(this, 'app', apps).onChange(() => {window.location.href = window.location.origin + window.location.pathname + '?' + this.app});
         this.gui.add(this, 'x').min(-1).max(1).step(0.01).onChange(this.applyGuiChanges);
         this.gui.add(this, 'y').min(-1).max(1).step(0.01).onChange(this.applyGuiChanges);
         this.gui.add(this, 'z').min(-1).max(1).step(0.01).onChange(this.applyGuiChanges);
